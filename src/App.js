a8bd9b89e57b4fd6857dc580756f7a16c616d944
Vladimir Smykovskiy <vladimireur@gmail.com>
2022-10-05 22:51:58 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect, useState } from 'react'
+import Pagination from './components/Pagination'
+import Posts from './components/Posts'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ posts, setPosts ] = useState( [] )
+    const [ loading, setLoading ] = useState( false )
+    const [ currentPage, setCurrentPage ] = useState( 1 )
+    const [ postsPerPage, setPostsPerPage ] = useState( 10 )
+
+    useEffect( () => {
+        const fetchPosts = async() => {
+            setLoading( true )
+            const res = await axios.get( 'https://jsonplaceholder.typicode.com/posts' )
+            setPosts( res.data )
+            setLoading( false )
+        }
+        fetchPosts()
+    }, [] )
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage        // индекс последнего поста выбранной страницы
+    const indexOfFirstPost = indexOfLastPost - postsPerPage   // индекс первого поста выбранной страницы
+    const currentPosts = posts.slice( indexOfFirstPost, indexOfLastPost ) // вырезаем нужные данные
+
+    // Change page
+    const paginate = pageNumber => {
+        setCurrentPage( pageNumber )
+    }
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-primary mb-3">My Blog</h1>
+            <Posts posts={ currentPosts } loading={ loading }/>
+            <Pagination
+                postsPerPage={ postsPerPage }
+                totalPosts={ posts.length }
+                paginate={ paginate }/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,25 @@
+const Pagination = ( { postsPerPage, totalPosts, paginate } ) => {
+    const pageNumbers = []
+    for( let i = 1; i <= Math.ceil( totalPosts / postsPerPage ); i++ ) {
+        pageNumbers.push( i )
+    }
+    return (
+        <nav>
+            <ul className="pagination">
+                { pageNumbers.map( num =>
+                    <li key={ num } className="page-item">
+                        <a
+                            onClick={ () => paginate( num ) }
+                            href="!#"
+                            className="page-link"
+                        >
+                            { num }
+                        </a>
+                    </li>
+                ) }
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
